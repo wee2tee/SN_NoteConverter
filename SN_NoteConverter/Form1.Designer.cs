2d045c3c750039b3218dc8d7bef3355eeaab3aa1
Weerawat Tarujettanarom <weerawat.36@gmail.com>
2018-02-12 17:45:36 +0700

    Test Commit.

--- SN_NoteConverter/Form1.Designer.cs
+++ SN_NoteConverter/Form1.Designer.cs
@@ -28,12 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgv1 = new CC.XDatagrid();
+            this.dgv2 = new CC.XDatagrid();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnLoadOldData = new System.Windows.Forms.Button();
+            this.btnStartImport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowSortByColumnHeaderClicked = false;
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.AllowUserToDeleteRows = false;
+            this.dgv1.AllowUserToResizeColumns = false;
+            this.dgv1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(207)))), ((int)(((byte)(179)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgv1.EnableHeadersVisualStyles = false;
+            this.dgv1.FillEmptyRow = false;
+            this.dgv1.FocusedRowBorderRedLine = false;
+            this.dgv1.Location = new System.Drawing.Point(3, 3);
+            this.dgv1.MultiSelect = false;
+            this.dgv1.Name = "dgv1";
+            this.dgv1.ReadOnly = true;
+            this.dgv1.RowHeadersVisible = false;
+            this.dgv1.RowTemplate.Height = 26;
+            this.dgv1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv1.Size = new System.Drawing.Size(426, 441);
+            this.dgv1.StandardTab = true;
+            this.dgv1.TabIndex = 0;
+            // 
+            // dgv2
+            // 
+            this.dgv2.AllowSortByColumnHeaderClicked = false;
+            this.dgv2.AllowUserToAddRows = false;
+            this.dgv2.AllowUserToDeleteRows = false;
+            this.dgv2.AllowUserToResizeColumns = false;
+            this.dgv2.AllowUserToResizeRows = false;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(207)))), ((int)(((byte)(179)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv2.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgv2.EnableHeadersVisualStyles = false;
+            this.dgv2.FillEmptyRow = false;
+            this.dgv2.FocusedRowBorderRedLine = false;
+            this.dgv2.Location = new System.Drawing.Point(435, 3);
+            this.dgv2.MultiSelect = false;
+            this.dgv2.Name = "dgv2";
+            this.dgv2.ReadOnly = true;
+            this.dgv2.RowHeadersVisible = false;
+            this.dgv2.RowTemplate.Height = 26;
+            this.dgv2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv2.Size = new System.Drawing.Size(426, 441);
+            this.dgv2.StandardTab = true;
+            this.dgv2.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dgv1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgv2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 67);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(864, 447);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(27, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(214, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnLoadOldData
+            // 
+            this.btnLoadOldData.Enabled = false;
+            this.btnLoadOldData.Location = new System.Drawing.Point(247, 20);
+            this.btnLoadOldData.Name = "btnLoadOldData";
+            this.btnLoadOldData.Size = new System.Drawing.Size(117, 25);
+            this.btnLoadOldData.TabIndex = 5;
+            this.btnLoadOldData.Text = "Load Old Data";
+            this.btnLoadOldData.UseVisualStyleBackColor = true;
+            this.btnLoadOldData.Click += new System.EventHandler(this.btnLoadOldData_Click);
+            // 
+            // btnStartImport
+            // 
+            this.btnStartImport.Enabled = false;
+            this.btnStartImport.Location = new System.Drawing.Point(370, 20);
+            this.btnStartImport.Name = "btnStartImport";
+            this.btnStartImport.Size = new System.Drawing.Size(90, 25);
+            this.btnStartImport.TabIndex = 5;
+            this.btnStartImport.Text = "Start Import";
+            this.btnStartImport.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 517);
+            this.Controls.Add(this.btnStartImport);
+            this.Controls.Add(this.btnLoadOldData);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private CC.XDatagrid dgv1;
+        private CC.XDatagrid dgv2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnLoadOldData;
+        private System.Windows.Forms.Button btnStartImport;
     }
 }
 
--- SN_NoteConverter/Form1.cs
+++ SN_NoteConverter/Form1.cs
@@ -6,14 +6,84 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using CC;
+using SN_NoteConverter.Model;
+//using SN_NoteConverter.Model2;
 
 namespace SN_NoteConverter
 {
     public partial class Form1 : Form
     {
+        private enum TableName
+        {
+            NOTE_ISTAB,
+            NOTE,
+            NOTE_COMMENT,
+            NOTE_CALENDAR,
+            EVENT_CALENDAR,
+            TRAINING_CALENDAR
+        }
+
+        private TableName tbl_name;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Enum.GetValues(typeof(TableName)).Cast<TableName>().ToList().ForEach(i => this.comboBox1.Items.Add(new XDropdownListItem { Text = i.ToString(), Value = i }));
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.tbl_name = (TableName)((XDropdownListItem)((ComboBox)sender).SelectedItem).Value;
+            this.btnLoadOldData.Enabled = true;
+        }
+
+        private void btnLoadOldData_Click(object sender, EventArgs e)
+        {
+            using (sn_netEntities sn = new sn_netEntities())
+            {
+                switch (this.tbl_name)
+                {
+                    case TableName.NOTE_ISTAB:
+                        BindingList<istab> l1 = new BindingList<istab>(sn.istab.Where(i => i.tabtyp == "06" || i.tabtyp == "07").ToList());
+                        this.dgv1.DataSource = l1;
+                        break;
+                    case TableName.NOTE:
+                        BindingList<SupportNote_Min> l2 = new BindingList<SupportNote_Min>(sn.support_note.Select(s => new SupportNote_Min { id = s.id, date = s.date, username = s.users_name }).ToList());
+                        this.dgv1.DataSource = l2;
+                        break;
+                    case TableName.NOTE_COMMENT:
+                        BindingList<support_note_comment> l3 = new BindingList<support_note_comment>(sn.support_note_comment.ToList());
+                        this.dgv1.DataSource = l3;
+                        break;
+                    case TableName.NOTE_CALENDAR:
+                        BindingList<note_calendar> l4 = new BindingList<note_calendar>(sn.note_calendar.ToList());
+                        this.dgv1.DataSource = l4;
+                        break;
+                    case TableName.EVENT_CALENDAR:
+                        BindingList<event_calendar> l5 = new BindingList<event_calendar>(sn.event_calendar.ToList());
+                        this.dgv1.DataSource = l5;
+                        break;
+                    case TableName.TRAINING_CALENDAR:
+                        BindingList<training_calendar> l6 = new BindingList<training_calendar>(sn.training_calendar.ToList());
+                        this.dgv1.DataSource = l6;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    public class SupportNote_Min
+    {
+        public int id { get; set; }
+        public DateTime? date { get; set; }
+        public string username { get; set; }
+        //public support_note support_note { get; set; }
     }
 }
